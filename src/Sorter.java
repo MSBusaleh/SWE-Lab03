52c8e30d996745b97bf5f291d77a1ba93597b1e8
Lanykiny <a.e.mayman@hotmail.com>
2023-09-22 17:22:59 +0300

    Insertion sort

--- src/Sorter.java
+++ src/Sorter.java
@@ -1,22 +1,15 @@
 public class Sorter {
-    public void sort(int[] arr)
-    {
-        int n = arr.length;
-
-        // One by one move boundary of unsorted subarray
-        for (int i = 0; i < n - 1; i++) {
-            // Find the minimum element in unsorted array
-            int min_idx = i;
-            for (int j = i + 1; j < n; j++) {
-                if (arr[j] < arr[min_idx])
-                    min_idx = j;
+    public static void insertionSort(int array[]) {
+        int n = array.length;
+        for (int i = 1; i < n; i++) {
+            int key = array[i];
+            int j = i - 1;
+            while ((j > -1) && (array[j] > key)) {
+                array[j + 1] = array[j];
+                j--;
             }
-
-            // Swap the found minimum element with the first
-            // element
-            int temp = arr[min_idx];
-            arr[min_idx] = arr[i];
-            arr[i] = temp;
+            array[j + 1] = key;
         }
     }
 }
+
